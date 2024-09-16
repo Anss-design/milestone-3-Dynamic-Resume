4e42f88d1ff49422cd0d0ea0d0dd75307868b1a1
Muhammad Anss <ansssaleem.nws@gmail.com>
2024-09-16 12:14:22 +0500

    script.ts

--- script.ts
+++ script.ts
@@ -1,33 +1,44 @@
 // listing element
 
-document.getElementById('resumeform')?.addEventListener('submit', function(event) {
+document.getElementById('resumeform')?.addEventListener('submit', function (event) {
     event.preventDefault();
 
 
     // type assertion
+    const profilepictureinput = document.getElementById('profilepicture') as HTMLInputElement;
+
     const nameElement = document.getElementById('name') as HTMLInputElement;
     const emailElement = document.getElementById('email') as HTMLInputElement;
     const phoneElement = document.getElementById('phone') as HTMLInputElement;
+    const addressElement = document.getElementById('address') as HTMLInputElement;
     const educationElement = document.getElementById('education') as HTMLInputElement;
     const experienceElement = document.getElementById('experience') as HTMLInputElement;
     const skillsElement = document.getElementById('skills') as HTMLInputElement;
 
 
-    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
-        
+    if (profilepictureinput && nameElement && emailElement && phoneElement && addressElement && educationElement && experienceElement && skillsElement) {
+
         const name = nameElement.value;
         const email = emailElement.value;
         const phone = phoneElement.value;
+        const address = addressElement.value;
         const education = educationElement.value;
         const experience = experienceElement.value;
         const skills = skillsElement.value;
-    
-    // creating resume output
-    const resumeOutput = `
+
+        // picture elements
+        const profilepicturefile = profilepictureinput.files?.[0];
+        const profilepictureurl = profilepicturefile ? URL.createObjectURL(profilepicturefile) : '';
+
+
+        // creating resume output
+        const resumeOutput = `
     <h2>Resume</h2>
+    ${profilepictureurl ? `<img src="${profilepictureurl}" alt="profile Picture" class="profilePicture">` : ''}
     <p><strong>Name:</strong> ${name}</p>
     <p><strong>Email:</strong> ${email}</p>
     <p><strong>Phone:</strong> ${phone}</p>
+    <p><strong>Address:</strong> ${address}</p>
     <h3>Education</h3>
     <p>${education}</p>
     <h3>Experience</h3>
@@ -35,12 +46,12 @@ document.getElementById('resumeform')?.addEventListener('submit', function(event
     <h3>Skills</h3>
     <p>${skills}</p>
     `;
-    
-    const resumeOutputElement = document.getElementById('resumeoutput');
-    if (resumeOutputElement) {
-        resumeOutputElement.innerHTML = resumeOutput;
-    } else{
-        console.error('Resume output element not found');
+
+        const resumeOutputElement = document.getElementById('resumeoutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        } else {
+            console.error('Resume output element not found');
+        }
     }
-}
 })
